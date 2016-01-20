a1d39961556975bc4bb110fa1592ef5795b2fdc5
Anders Einar Hilden <hildenae@gmail.com>
2016-01-20 22:07:47 +0100

    Proper download of commonscrawl data

---
+++ commoncrawl/get-no.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+cd ../storage/cdx-index-client
+# Index list from http://index.commoncrawl.org/
+for INDEX in CC-MAIN-2014-52 CC-MAIN-2015-06 CC-MAIN-2015-11 CC-MAIN-2015-14 CC-MAIN-2015-18 CC-MAIN-2015-22 CC-MAIN-2015-27 CC-MAIN-2015-32 CC-MAIN-2015-35 CC-MAIN-2015-40 CC-MAIN-2015-48
+do mkdir -p $INDEX; cd $INDEX; ../cdx-index-client.py -p 4 -c $INDEX '*.no' --fl url -z; cd ..;
+done;
+
