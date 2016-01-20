a774ec008e9287979dd126f5e0e61670990dc6a8
Anders Einar Hilden <hildenae@gmail.com>
2016-01-20 22:38:16 +0100

    commoncrawl/get-unsorted.sh is ran from Makefile, so use correct path

--- commoncrawl/get-unsorted.sh
+++ commoncrawl/get-unsorted.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-for FILENAME in $(find ../storage/cdx-index-client/ -type f -name '*.gz'); 
+for FILENAME in $(find ./storage/cdx-index-client/ -type f -name '*.gz'); 
 	do 
 		zcat "$FILENAME" | ../default_extract; 
 done
