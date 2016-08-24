efd84b59bb75f382234833e64d2e9c99e9258d5b
Anders Einar Hilden <hildenae@gmail.com>
2016-08-24 15:47:09 +0200

    update wb

--- wayback/get_domains.sh
+++ wayback/get_domains.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 for FILE in data-ok/*.json.7z; 
 do 
-	7z e -so -bd $FILE 2>/dev/null | ../dotkatno | sort | uniq > data-uniq/$(basename $FILE ".7z"); 
+	echo "Getting domains from $FILE";
+	7z e -so -bd $FILE 2>/dev/null | ../urldecode | ../default_extract > data-uniq/$(basename $FILE ".7z"); 
 done;
 
-#cat data-uniq/*.json | sort | uniq > wayback.domains
+cat data-uniq/*.json | sort | uniq > wayback.domains
